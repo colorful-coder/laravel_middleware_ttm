40f4dd490afeb7fef53f372d4a9b05710a5e5a98
scm.thinthinmoe@gmail.com <scm.thinthinmoe@gmail.com>
2020-07-23 20:34:07 +0630

    add facades

---
+++ app/Providers/TestFacadesServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App;
+use Illuminate\Support\ServiceProvider;
+use App\Test\TestFacades as TestFacades;
+
+class TestFacadesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        App::bind('test', function () {
+            return new TestFacades;
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,20 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test-facade', function () {
+    return TestFacades::testingFacades();
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('profile/{id}',["uses"=>"HomeController@checkMD","middleware"=>"check.email"])->name('profile');
-Route::group(['middleware'=>['role']],function(){
+Route::get('profile/{id}', ["uses" => "HomeController@checkMD", "middleware" => "check.email"])->name('profile');
+Route::group(['middleware' => ['role']], function () {
     Route::get('/list', 'HomeController@userList');
 });
